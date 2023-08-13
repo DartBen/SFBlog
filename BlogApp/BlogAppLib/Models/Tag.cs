d42df2f0ddce75585bc41cffc64bfafa5970c691
DartBen <ryabchikk@mail.ru>
2023-08-13 15:49:19 +0300

    Tag init

---
+++ BlogApp/BlogAppLib/Models/Tag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAppLib.Models
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string TagName { get; set; }
+
+        // Связь с статьями
+        public List<Article> Articles { get; set; } = new List<Article>();
+    }
+}
