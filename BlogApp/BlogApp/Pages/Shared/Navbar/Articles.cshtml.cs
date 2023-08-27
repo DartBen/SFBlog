ca365b35fd72f3dee0c1d272c1a74a1ea51f6604
DartBen <ryabchikk@mail.ru>
2023-08-27 14:26:30 +0300

    В меню добавлены основные страницы (по одной на контроллер)

---
+++ BlogApp/BlogApp/Pages/Shared/Navbar/Articles.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlogApp.Pages.Shared.Navbar
+{
+    public class ArticlesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
